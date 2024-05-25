1be61a87c6e3fb070cf611574236013c5f0f81bc
IsNicolasB <isma.nico7842018@gmail.com>
2024-05-25 19:58:33 -0300

    Parte N°1 - Finalización Universidad - Solo con bootstrap sin CSS

--- src/main/java/ar/edu/unju/fi/controller/IndexController.java
+++ src/main/java/ar/edu/unju/fi/controller/IndexController.java
@@ -5,11 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/principal")
+@RequestMapping("/main")
 public class IndexController {
 	@GetMapping("/index")
 	public String getIndex() {
 		return "index";
-	}
-	
+	}	
 }
